77689193a9a52e42c3def23b3a822e289d25990a
JacksonTav <jacksontav@hotmail.com>
2021-07-03 12:12:59 -0300

    Refatoração do código

---
+++ ApiCatalogoJogos/Entities/Jogo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Entities
+{
+    public class Jogo
+    {
+        public Jogo(Guid guid, string v1, string v2, double v3)
+        {
+            Guid = guid;
+            V1 = v1;
+            V2 = v2;
+            V3 = v3;
+        }
+
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Produtora { get; set; }
+        public double Preco { get; set; }
+        public Guid Guid { get; }
+        public string V1 { get; }
+        public string V2 { get; }
+        public double V3 { get; }
+    }
+}
---
+++ ApiCatalogoJogos/ViewModel/JogoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.ViewModel
+{
+    public class JogoViewModel
+    {
+        public JogoViewModel(Guid id, string v1, string v2, double v3)
+        {
+        }
+
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Produtora { get; set; }
+        public double Preco { get; set; }
+        
+    }
+}
